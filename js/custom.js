556cad32f349ceb3fd9dcd16b91fb6857dfdaabd
jakeleslie <43657120+jakeleslie@users.noreply.github.com>
2021-09-30 09:57:54 -0400

    need to reload api data on page refresh

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,10 @@
-const players = 'https://www.balldontlie.io/api/v1/season_averages?season=2018&player_ids[]=1&player_ids[]=2' //this will get all players
+const players = 'https://www.balldontlie.io/api/v1/season_averages?season=2018&player_ids[]=237&player_ids[]=2' //this will get all players
 const teamStats = 'https://www.balldontlie.io/api/v1/teams'
 const seasonAverages = 'https://www.balldontlie.io/api/v1/season_averages'
 
-function fetchLeftData() {
-    fetch(players)
+document.addEventListener("DOMContentLoaded", function(){
+//function fetchLeftData(){
+fetch(players)
         .then(response => {
             if (!response.ok){
                throw Error("ERROR")
@@ -15,7 +16,11 @@ function fetchLeftData() {
             console.log(data.data) //have to change this so that i can just get a statistic 
             const html = data.data
             .map(user => { //gets a map of all of the queried players first names. 
-                return `<p>Assists: ${user.ast}</p>`
+                return `<p>Assists: ${user.pts} 
+                FGM: ${user.fgm}
+                ASSISTS: ${user.ast}
+                </p>`
+                
             })
             .join("") //makes it a bunch of paragraph tags instead of array
             
@@ -27,9 +32,12 @@ function fetchLeftData() {
             alert("There was an error requesting data.\n" + error);
             
         })
-}
+   // }
+})
 
-function fetchMiddleData() {
+document.addEventListener("DOMContentLoaded", function(){
+    function fetchMiddleData(){
+
     fetch(teamStats)
         .then(response => {
             if (!response.ok){
@@ -53,9 +61,12 @@ function fetchMiddleData() {
             console.log(error) //alert for error 
             alert("There was an error requesting data.\n" + error);
         })
-}
+    }
+    
+    fetchMiddleData()
+})
 
-function fetchRightData() {
+document.addEventListener("DOMContentLoaded", function(){
     fetch(seasonAverages)
         .then(response => {
             if (!response.ok){
@@ -78,9 +89,7 @@ function fetchRightData() {
             console.log(error) //alert for error 
             alert("There was an error requesting data.\n" + error);
         })
-}
-window.addEventListener('load', (event) => { //new calls on each load 
+
+})
 fetchLeftData()
-fetchMiddleData()
-fetchRightData() 
-}) //Everytime page loads api data is loaded from api 
+// fetchRightData() 
