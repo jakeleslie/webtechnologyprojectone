04f63ecea5ab99f663549814c618cfbbbcb9dd1b
jakeleslie <43657120+jakeleslie@users.noreply.github.com>
2021-10-01 00:25:49 -0400

    Much needed changes to the javascript have been made

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
-const players = 'https://www.balldontlie.io/api/v1/season_averages?season=2018&player_ids[]=237' //this will get all players
-const teamStats = 'https://www.balldontlie.io/api/v1/teams'
-const seasonAverages = 'https://www.balldontlie.io/api/v1/players'
+// const players = 'https://www.balldontlie.io/api/v1/season_averages?season=2018&player_ids[]=237' //this will get all players
+// const teamStats = 'https://www.balldontlie.io/api/v1/teams'
+// const seasonAverages = 'https://www.balldontlie.io/api/v1/players'
 
-var ps = fetch('https://www.balldontlie.io/api/v1/season_averages?season=2018&player_ids[]=237')
-var ts = fetch('https://www.balldontlie.io/api/v1/teams')
+var ps = fetch('https://www.balldontlie.io/api/v1/season_averages?season=2020&player_ids[]=237')
+var ts = fetch('https://www.balldontlie.io/api/v1/games/1')
 var sa = fetch('https://www.balldontlie.io/api/v1/players')
 document.addEventListener("DOMContentLoaded", function(){
     ps
@@ -16,9 +16,16 @@ document.addEventListener("DOMContentLoaded", function(){
         .then(data => {
             console.log(data.data) //have to change this so that i can just get a statistic 
             const html = data.data
+            
             .map(user => { //gets a map of all of the queried players first names. 
-                return `<h5>Lebron James 2018 Averages</h5>
-                <p>Average points per game ${user.pts}</p>`
+                
+               
+                return `<h3 class="text-center" id="innerdivheader">Lebron James 2020 Averages</h3>
+                <p id="leftStyle">Average points per game: ${user.pts}</p>
+                <p id="leftStyle">Average assists per game: ${user.ast}</p>
+                <p id="leftStyle">Total games played: ${user.games_played}</p>
+                <p id="leftStyle">Per-minute point rating in 36 minutes: ${36 * user.pts / 35.13}</p>`
+                //wanted to be able to implement it by diving by user.min and having only two decminal places
             })
             .join("") //makes it a bunch of paragraph tags instead of array
             
@@ -40,26 +47,26 @@ document.addEventListener("DOMContentLoaded", function(){
             return response.json()
         })
         .then(data => {
-            console.log(data.data)
-            const html = data.data
-            .map(user => {
-                return `<h5>Lakers averages 2018</h5>
-                <p>Team: ${user.city}</p>`
-            })
-            .join("") //makes it a bunch of paragraph tags instead of array
+            console.log(data)
+            const html = data
+            document.querySelector('#contentMid').innerHTML = `<h3>${data.date}<h3>
+            <h3>${data.home_team_score}</h3>`
             
-            console.log(html)
-            document.querySelector('#contentMid').innerHTML = html
+           
+            
+            // .map(user => {
+            //     return `<h5>Lakers averages 2018</h5>
+            //     <p>Team: ${user.abbreviation}</p>`
+            // })
+            // .join("") //makes it a bunch of paragraph tags instead of array
+            
+            // console.log(html)
+            
         })
         .catch(error => {
             console.log(error) //alert for error 
             alert("There was an error requesting data.\n" + error);
         })
-    
-    //can do event listeneter for conetent loaded to print out new stuff
-    //On
-    
-   
 })
 
 document.addEventListener("DOMContentLoaded", function(){
