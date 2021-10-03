d47ebee967b724c11e5a40ec560896ec9b6cc510
jakeleslie <43657120+jakeleslie@users.noreply.github.com>
2021-10-03 16:48:31 -0400

    All done

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,11 @@
 var ps = fetch('https://www.balldontlie.io/api/v1/season_averages?season=2020&player_ids[]=237')
-var ts = fetch('https://www.balldontlie.io/api/v1/games/1')
-var sa = fetch('https://www.balldontlie.io/api/v1/players')
+let randomGame = Math.random() * (5000 - 1) + 1
+let t = randomGame.toString()
+let ts = fetch('https://www.balldontlie.io/api/v1/games/' + t)
+
+let s = randomPlayer = Math.random() * (25 - 1) + 1
+let playerS = s.toString()
+var sa = fetch('https://www.balldontlie.io/api/v1/players/' + playerS) //random player spotlight 
 document.addEventListener("DOMContentLoaded", function(){
     ps
         .then(response => {
@@ -44,13 +49,12 @@ document.addEventListener("DOMContentLoaded", function(){
             console.log(data)
             const html = data
             document.querySelector('#contentMid').innerHTML = `
-            <h3 class="text-center">Most Recent game</h3>
-            <p> Home team: ${data.home_team.abbreviation} Away team: ${data.visitor_team.abbreviation}
-            <p>Date: ${data.date}<p>
-            <p>Home team score: ${data.home_team_score}</p>
-            <p>Visiting team score: ${data.visitor_team_score}</p>
-            <p> Point difference: ${data.home_team_score - data.visitor_team_score}</p>
-            `
+            <h3 class="text-center">Game Stats</h3>
+            <p id="midStyle"> Home team: ${data.home_team.abbreviation} | Away team: ${data.visitor_team.abbreviation}
+            <p id="midStyle">${data.home_team.abbreviation} score: ${data.home_team_score}</p>
+            <p id="midStyle">${data.visitor_team.abbreviation} score: ${data.visitor_team_score}</p>
+            <p id="midStyle"> Point difference for home team: ${data.home_team_score - data.visitor_team_score}</p>
+            <p id="midStyle"> ${(data.home_team_score > data.visitor_team_score) ? data.home_team.abbreviation : data.visitor_team.abbreviation} are the winners!</p>`
             
         })
         .catch(error => {
@@ -60,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 document.addEventListener("DOMContentLoaded", function(){
-   sa
+    sa
         .then(response => {
             if (!response.ok){
                throw Error("ERROR")
@@ -68,18 +72,16 @@ document.addEventListener("DOMContentLoaded", function(){
             return response.json()
         })
         .then(data => {
-            console.log(data.data)
-            const html = data.data
-            .map(user => {
-                return `<p>fname: ${user.first_name}
-                            lname: ${user.last_name}
-                </p>`
-            })
-            .join("") //makes it a bunch of paragraph tags instead of array
+            console.log(data)
+            const html = data
+            document.querySelector('#contentRight').innerHTML = `
+            <h3 class="text-center">Player Info</h3>
+            <p id="leftStyle">Name: ${data.first_name + " " + data.last_name}</p>
+            <p id="leftStyle">Position: ${data.position} 
+            <p id="leftStyle">Team: ${data.team.city + " " + data.team.name} 
+            <p id="leftStyle"> Conference: ${data.team.conference}</p>
+            <p id="leftStyle"> ${(data.weight_pounds != null) ? 'Weight: ' + data.weight_pounds : 'Weight: Unavailable' }</p>`
             
-            console.log(html)
-            document.querySelector('#contentRight').innerHTML = html
-
         })
         .catch(error => {
             console.log(error) //alert for error 
